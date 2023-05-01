34b3c9b371a717165fe8f75f741b083178d07c0d
garipovroma <devilgar@gmail.com>
2023-05-02 01:17:36 +0400

    new models and config uploaded

--- MetaLearning-GAN/meta_gan/Models.py
+++ MetaLearning-GAN/meta_gan/Models.py
@@ -87,6 +87,52 @@ class Discriminator(nn.Module):
         result = self.fc(concat.squeeze())
         return result
 
+class DeepSetFlattenedModel(torch.nn.Module):
+    def __init__(self, hidden_size_0=1, hidden_size_1=1, predlast_hidden_size=1, meta_size = 27, out_classes=1):
+        super().__init__()
+        self.hidden_size_0 = hidden_size_0
+        self.hidden_size_1 = hidden_size_1
+        self.predlast_hidden_size = predlast_hidden_size
+        self.meta_size = meta_size
+        self.out_classes = out_classes
+        self.inv_0 = deepsetlayers.InvLinear(hidden_size_0, hidden_size_1)
+        self.equiv_0 = deepsetlayers.EquivLinear(1, hidden_size_0)
+        self.relu = torch.nn.ReLU()
+        self.classifier = torch.nn.Sequential(
+            torch.nn.Linear(hidden_size_1 + meta_size, 2 * predlast_hidden_size),
+            torch.nn.BatchNorm1d(2 * predlast_hidden_size),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(CONFIG.FIRST_DROPOUT_RATE),
+            torch.nn.Linear(2 * predlast_hidden_size, predlast_hidden_size),
+            torch.nn.BatchNorm1d(predlast_hidden_size),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(CONFIG.SECOND_DROPOUT_RATE),
+            torch.nn.Linear(predlast_hidden_size, out_classes),
+        )
+
+    def forward(self, X, y):
+        # (batch_size, N, M)
+        X = X.unsqueeze(-1)
+        X = X.flatten(1, 2)
+        # (batch_size, N * M, 1)
+        X = self.equiv_0(X)
+        # (batch_size, N * M, hidden_size_0)
+        X = self.inv_0(X)
+        # (batch_size, hidden_size_1)
+
+        y = y.view(-1, self.meta_size)
+        # (batch_size, meta_size)
+
+        # vec0 = torch.abs(pos_vec - neg_vec)
+        # vec1 = pos_vec + neg_vec
+        x = torch.hstack([X, y])
+        # x = torch.hstack([vec0, vec1, y])
+
+        # (batch_size, hidden_size_1 + meta_size)
+        x = self.classifier(x)
+        # (batch_size, out_classes)
+        return x
+
 class CONFIG:
     # DATA
     features = 16
@@ -189,3 +235,112 @@ class DeepSetModelV5(torch.nn.Module):
         x = self.regressor(x)
         # (batch_size, out_classes)
         return x
+
+
+class DeepSetModelV6(torch.nn.Module):
+    def __init__(self, hidden_size_0=1, hidden_size_1=1, predlast_hidden_size=1, meta_size=27, out_classes=1):
+        super().__init__()
+        self.hidden_size_0 = hidden_size_0
+        self.hidden_size_1 = hidden_size_1
+        self.predlast_hidden_size = predlast_hidden_size
+        self.meta_size = meta_size
+        self.out_classes = out_classes
+
+        # pos
+        self.inv_0_pos = deepsetlayers.InvLinear(hidden_size_0, hidden_size_0)
+        self.inv_1_pos = deepsetlayers.InvLinear(hidden_size_1, 3 * hidden_size_1 // 2)
+        self.equiv_0_pos = deepsetlayers.EquivLinear(1, hidden_size_0)
+        self.equiv_1_pos = deepsetlayers.EquivLinear(hidden_size_0, hidden_size_1)
+
+        # neg
+        self.inv_0_neg = deepsetlayers.InvLinear(hidden_size_0, hidden_size_0)
+        self.inv_1_neg = deepsetlayers.InvLinear(hidden_size_1, 3 * hidden_size_1 // 2)
+        self.equiv_0_neg = deepsetlayers.EquivLinear(1, hidden_size_0)
+        self.equiv_1_neg = deepsetlayers.EquivLinear(hidden_size_0, hidden_size_1)
+
+        # all
+        self.inv_0_all = deepsetlayers.InvLinear(hidden_size_0, hidden_size_0)
+        self.inv_1_all = deepsetlayers.InvLinear(hidden_size_1, 3 * hidden_size_1 // 2)
+        self.equiv_0_all = deepsetlayers.EquivLinear(1, hidden_size_0)
+        self.equiv_1_all = deepsetlayers.EquivLinear(hidden_size_0, hidden_size_1)
+
+        self.relu = torch.nn.ReLU()
+        self.classifier = torch.nn.Sequential(
+            torch.nn.Linear(3 * (3 * hidden_size_1 // 2) + meta_size, 2 * predlast_hidden_size),
+            torch.nn.BatchNorm1d(2 * predlast_hidden_size),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(CONFIG.FIRST_DROPOUT_RATE),
+            torch.nn.Linear(2 * predlast_hidden_size, predlast_hidden_size),
+            torch.nn.BatchNorm1d(predlast_hidden_size),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(CONFIG.SECOND_DROPOUT_RATE),
+            torch.nn.Linear(predlast_hidden_size, out_classes),
+        )
+
+    def _forward(self, x, mode='all'):
+        if mode == 'pos':
+            equiv_0 = self.equiv_0_pos
+            equiv_1 = self.equiv_1_pos
+            inv_0 = self.inv_0_pos
+            inv_1 = self.inv_1_pos
+        elif mode == 'neg':
+            equiv_0 = self.equiv_0_neg
+            equiv_1 = self.equiv_1_neg
+            inv_0 = self.inv_0_neg
+            inv_1 = self.inv_1_neg
+        elif mode == 'all':
+            equiv_0 = self.equiv_0_all
+            equiv_1 = self.equiv_1_all
+            inv_0 = self.inv_0_all
+            inv_1 = self.inv_1_all
+
+        x = x.unsqueeze(-1)
+        # (batch_size, N, M, 1)
+        N = x.shape[1]
+        x = x.flatten(0, 1)
+        # (batch_size * N, M, 1)
+        x = equiv_0(x)
+        # (batch_size * N, M, hidden_size_0)
+        x = self.relu(x)
+        x = inv_0(x)
+        # (batch_size * N, hidden_size_0)
+        x = x.reshape(-1, N, self.hidden_size_0)
+        # (batch_size, N, hidden_size_0)
+
+        x = equiv_1(x)
+        # (batch_size, N, hidden_size_1)
+        x = self.relu(x)
+        x = inv_1(x)
+        # (batch_size, hidden_size_1)
+        return x
+
+    def forward(self, pos, neg, y):
+
+        pos_vec = self._forward(pos, 'pos')
+        neg_vec = self._forward(neg, 'neg')
+
+        all = torch.cat([pos, neg], dim=2)
+        all_vec = self._forward(all, 'all')
+
+        y = y.view(-1, self.meta_size)
+
+        # (batch_size, meta_size)
+
+        # vec0 = torch.abs(pos_vec - neg_vec)
+        # vec1 = pos_vec + neg_vec
+        x = torch.hstack([pos_vec, neg_vec, all_vec, y])
+        # x = torch.hstack([vec0, vec1, y])
+
+        # (batch_size, hidden_size_1 + meta_size)
+        x = self.classifier(x)
+        # (batch_size, out_classes)
+        return x
+
+def get_discriminator():
+    features = CONFIG.features
+    instances = CONFIG.instances
+    classes = CONFIG.classes
+    metas_length = CONFIG.meta_size
+    lambdas_length = CONFIG.out_classes
+    discriminator = Discriminator(features, instances, classes, metas_length, lambdas_length)
+    return discriminator
